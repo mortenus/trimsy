ff9f8283d45d7e6359d53aeba0c72aae9509d239
mortenus <morten.hihiji96@gmail.com>
2022-12-21 20:33:17 +0100

    Fixing5

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import styles from '../styles/Home.module.scss';
 
 import { Accomplish, ContactForm, Influence, Intro, Product } from 'containers/Home';
-import { Header } from 'components';
 
 export default function Home() {
   return (
