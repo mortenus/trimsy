60d1916c7f4db76c39ef8d73c01a02166ac68020
mortenus <morten.hihiji96@gmail.com>
2022-12-29 16:27:35 +0100

    Fixed

--- containers/Home/Accomplish/index.tsx
+++ containers/Home/Accomplish/index.tsx
@@ -50,27 +50,28 @@ const Accomplish = () => {
   //   };
 
   return (
-    <section
-      className={styles.accomplish}
-      ref={AccomplishRef}
-      style={{
-        marginTop:
-          scrollMove === false
-            ? '-50px'
-            : scrollMove === true
-            ? '-210px'
-            : `${-Math.abs(+scrollMove)}px`,
-      }}>
-      <div className={styles.text}>
-        <h2>Accomplish your goals in one go</h2>
-        <p>
-          Get a headstart on your journey with advanced functionalities and bring your vision to
-          life
-        </p>
+    <section className={styles.accomplish} ref={AccomplishRef}>
+      <div
+        className="wrapper"
+        style={{
+          marginTop:
+            scrollMove === false
+              ? '-50px'
+              : scrollMove === true
+              ? '-210px'
+              : `${-Math.abs(+scrollMove)}px`,
+        }}>
+        <div className={styles.text}>
+          <h2>Accomplish your goals in one go</h2>
+          <p>
+            Get a headstart on your journey with advanced functionalities and bring your vision to
+            life
+          </p>
+        </div>
+        <Button to="#contact" black>
+          Contact us
+        </Button>
       </div>
-      <Button to="#contact" black>
-        Contact us
-      </Button>
     </section>
   );
 };
