71be53e0c01edbcb659392ea185ec2150fceae21
unknown <morten.hihij96@gmail.com>
2023-02-03 18:35:44 +0200

    Google Tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router';
 import { Footer, FormOverflow, Header } from 'components';
 import type { NextComponentType } from 'next';
 import React from 'react';
+import Script from 'next/script';
 
 type CustomAppProps = AppProps & {
   Component: NextComponentType & { title: string };
@@ -36,6 +37,16 @@ export default function App({ Component, pageProps }: CustomAppProps) {
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
+
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WFBCBDV');
+`}
+      </Script>
+
       {router.pathname === '/stands-with-ukraine' ? (
         <Header clean />
       ) : (
