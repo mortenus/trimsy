470e131f6b3f8684239c11b06160d236ca8d6ddd
unknown <morten.hihij96@gmail.com>
2023-02-20 01:20:25 +0200

    disable scroll fix

--- hooks/useHideScrollOnTrue.ts
+++ hooks/useHideScrollOnTrue.ts
@@ -4,8 +4,14 @@ export default function useHideScrollOnTrue(stateBool: boolean) {
   React.useEffect(() => {
     if (stateBool) {
       document.body.style.setProperty('overflow-y', `hidden`);
+      document.ontouchmove = function (e) {
+        e.preventDefault();
+      };
     } else {
       document.body.style.setProperty('overflow-y', `scroll`);
+      document.ontouchmove = function (e) {
+        return true;
+      };
     }
   }, [stateBool]);
 }
