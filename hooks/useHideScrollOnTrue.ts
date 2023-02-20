3ed61f67e135ed58ca02c51d911bedbc8c89cec4
unknown <morten.hihij96@gmail.com>
2023-02-20 15:34:29 +0200

    fix for fix

--- hooks/useHideScrollOnTrue.ts
+++ hooks/useHideScrollOnTrue.ts
@@ -1,24 +1,17 @@
 import React from 'react';
 
 export default function useHideScrollOnTrue(stateBool: boolean) {
-  function preventDefault(e: any) {
-    e.preventDefault();
-  }
-
-  function disableScroll() {
-    document.body.addEventListener('touchmove', preventDefault, { passive: false });
-  }
-  function enableScroll() {
-    document.body.removeEventListener('touchmove', preventDefault);
-  }
-
   React.useEffect(() => {
+    function preventDefault(e: any) {
+      e.preventDefault();
+    }
+
     if (stateBool) {
       document.body.style.setProperty('overflow-y', `hidden !important`);
-      disableScroll();
+      document.body.addEventListener('touchmove', preventDefault, { passive: false });
     } else {
       document.body.style.removeProperty('overflow-y');
-      enableScroll();
+      document.body.removeEventListener('touchmove', preventDefault);
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
