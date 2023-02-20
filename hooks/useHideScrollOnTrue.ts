62d7f0432327e0e4802049cd0df0973027de0dfd
unknown <morten.hihij96@gmail.com>
2023-02-20 15:31:19 +0200

    Fix scrolling for safari

--- hooks/useHideScrollOnTrue.ts
+++ hooks/useHideScrollOnTrue.ts
@@ -1,17 +1,26 @@
 import React from 'react';
 
 export default function useHideScrollOnTrue(stateBool: boolean) {
+  function preventDefault(e: any) {
+    e.preventDefault();
+  }
+
+  function disableScroll() {
+    document.body.addEventListener('touchmove', preventDefault, { passive: false });
+  }
+  function enableScroll() {
+    document.body.removeEventListener('touchmove', preventDefault);
+  }
+
   React.useEffect(() => {
     if (stateBool) {
-      document.body.style.setProperty('overflow-y', `hidden`);
-      document.ontouchmove = function (e) {
-        e.preventDefault();
-      };
+      document.body.style.setProperty('overflow-y', `hidden !important`);
+      disableScroll();
     } else {
-      document.body.style.setProperty('overflow-y', `scroll`);
-      document.ontouchmove = function (e) {
-        return true;
-      };
+      document.body.style.removeProperty('overflow-y');
+      enableScroll();
     }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [stateBool]);
 }
