0ec201bcb61515c980152f612c8ca234df00504a
unknown <morten.hihij96@gmail.com>
2023-03-27 22:54:17 +0300

    Fix

--- pages/blog/hr-trends-2023/index.tsx
+++ pages/blog/hr-trends-2023/index.tsx
@@ -25,59 +25,6 @@ const info = {
     'The field of HR is constantly evolving, driven by advances in technology, changes in the workforce demographics, and shifting cultural norms. Here are some of the potential future developments in HR:',
 };
 
-const a = [
-  {
-    data: {
-      date: 'March 27, 2023',
-      title: 'Human Resources Trends 2023 ',
-      description:
-        'The field of HR is constantly evolving, driven by advances in technology, changes in the workforce demographics, and shifting cultural norms. Here are some of the potential future developments in HR:',
-      imgUrl: 'https://trimsy.org/uploads/1556542894632.jpeg',
-    },
-    slug: '/blog/hr-trends-2023',
-  },
-  {
-    data: {
-      date: 'March 14, 2023',
-      title: 'Why Website Security is Important for Your Business',
-      description:
-        'Website security is crucial for any business that operates online, regardless of its size or industry. Secure website creates strong connection between the users/audience because trust is one of the main ingredients of successful entity, whether online or offline.',
-      imgUrl: 'https://trimsy.org/uploads/1556542894631.jpg',
-    },
-    slug: '/blog/why-website-security-is-important-for-your-business',
-  },
-  {
-    data: {
-      date: 'March 12, 2023',
-      title: 'How To Create a Winning Social Media Strategy for Your Website',
-      description:
-        'Creating a winning social media strategy is a crucial component of any successful website marketing plan. It gives confident opportunities for a website grown and expansion.',
-      imgUrl: 'https://trimsy.org/uploads/1556542894629.jpg',
-    },
-    slug: '/blog/how-to-create-a-winning-social-media-strategy',
-  },
-  {
-    data: {
-      date: 'March 9, 2023',
-      title: 'Setting Up Google Analytics Fast and Easy',
-      description:
-        "Tracking website could give a valuable information on how it is used, how well website keeps visitors on different pages, amd even more. The importance of resource about visitors' actions while interacting with a page could play very important part for any future analysis of website's perfomance or improvements of UI/UX features.'",
-      imgUrl: 'https://trimsy.org/uploads/google_analytics_logo.webp',
-    },
-    slug: '/blog/setting-up-google-analytics-fast-and-easy',
-  },
-  {
-    data: {
-      date: 'March 2, 2023',
-      title: 'How To Improve SEO of your Website in 5 Steps',
-      description:
-        'When SEO is set up and used correctly – it could bring lots of new traffic to the web page. It is not complex to accomplish, while optimizing your website could play important part in boosting your search presence. Google receives 90% of all online searches. That’s why we’re going to talk about setting up SEO for Googling and connect to Google Search Console for detailed analysis.',
-      imgUrl: 'https://trimsy.org/uploads/1666542894623.jpg',
-    },
-    slug: '/blog/how-to-improve-seo-of-your-website-in-5-steps',
-  },
-];
-
 const nextToReadArr = [
   {
     date: 'March 9, 2023',
