2cf40b71b653fc478136530e15bc93b7921e397a
unknown <morten.hihij96@gmail.com>
2023-04-10 10:09:45 +0300

    fix

--- pages/blog/unlocking-the-power-of-progressive-web-apps/index.tsx
+++ pages/blog/unlocking-the-power-of-progressive-web-apps/index.tsx
@@ -6,7 +6,7 @@ import BlogWrapper from 'features/Blog/BlogWrapper';
 import styles from '../Post.module.scss';
 
 const info = {
-  title: 'Unlocking the Power of Progressive Web Apps',
+  title: 'Unlocking the Power of Progressive Web Apps: Benefits and Features Explained',
   headerImg: '/uploads/1556542894637.jpg',
   date: 'April 10, 2023',
   createdAt: '2023-04-10T12:00:00+0000',
@@ -31,11 +31,11 @@ const info = {
 
 const nextToReadArr = [
   {
-    date: 'March 2, 2023',
-    title: 'How To Improve SEO of your Website in 5 Steps',
-    description: `When SEO is set up and used correctly – it could bring lots of new traffic to the web page. It is not complex to accomplish, while optimizing your website could play important part in boosting your search presence. Google receives 90% of all online searches. That’s why we’re going to talk about setting up SEO for Googling and connect to Google Search Console for detailed analysis.`,
-    imgUrl: 'https://trimsy.org/uploads/1666542894623.jpg',
-    slug: '/blog/how-to-improve-seo-of-your-website-in-5-steps',
+    date: 'March 9, 2023',
+    title: 'Setting Up Google Analytics Fast and Easy',
+    description: `Tracking website could give a valuable information on how it is used, how well website keeps visitors on different pages, amd even more. The importance of resource about visitors' actions while interacting with a page could play very important part for any future analysis of website's perfomance or improvements of UI/UX features.`,
+    imgUrl: 'https://trimsy.org/uploads/google_analytics_logo.webp',
+    slug: '/blog/setting-up-google-analytics-fast-and-easy',
   },
   {
     date: 'March 14, 2023',
@@ -45,11 +45,11 @@ const nextToReadArr = [
     slug: '/blog/why-website-security-is-important-for-your-business',
   },
   {
-    date: 'March 9, 2023',
-    title: 'Setting Up Google Analytics Fast and Easy',
-    description: `Tracking website could give a valuable information on how it is used, how well website keeps visitors on different pages, amd even more. The importance of resource about visitors' actions while interacting with a page could play very important part for any future analysis of website's perfomance or improvements of UI/UX features.`,
-    imgUrl: 'https://trimsy.org/uploads/google_analytics_logo.webp',
-    slug: '/blog/setting-up-google-analytics-fast-and-easy',
+    date: 'March 12, 2023',
+    title: 'How To Create a Winning Social Media Strategy for Your Website',
+    description: `Creating a winning social media strategy is a crucial component of any successful website marketing plan. It gives confident opportunities for a website grown and expansion.`,
+    imgUrl: 'https://trimsy.org/uploads/1556542894629.jpg',
+    slug: '/blog/how-to-create-a-winning-social-media-strategy',
   },
 ];
 
@@ -68,8 +68,6 @@ export default function Post() {
         <p>
           Whilst using PWAs capabilities - it will retain users, not difficult to develop and more.
         </p>
-
-        <p></p>
 
         <h3>What is PWAs</h3>
         <p>
@@ -107,7 +105,7 @@ export default function Post() {
           user actions even when there is no network connection.
         </p>
 
-        <h4>Modern</h4>
+        <h4>It's Modern!</h4>
 
         <p>
           PWAs use modern web technologies to provide reliable, engaging and fast user experience,
@@ -127,7 +125,74 @@ export default function Post() {
           native app when installed on a user's device.
         </p>
 
-        <h3></h3>
+        <h3>Examples of PWA usage:</h3>
+        <h4>1. Uber</h4>
+        <div className={styles.img}>
+          <Image
+            width="0"
+            height="0"
+            sizes="100vw"
+            style={{ width: '75%', height: 'auto' }}
+            src={'/uploads/1556542894639.jpg'}
+            alt={'Image'}
+            loading={'lazy'}
+          />
+        </div>
+        <p>
+          Uber's PWA offers a streamlined experience for requesting rides and tracking their
+          progress, with features such as real-time updates and location tracking.
+        </p>
+
+        <h4>2. Flipboard</h4>
+        <div className={styles.img}>
+          <Image
+            width="0"
+            height="0"
+            sizes="100vw"
+            style={{ width: '75%', height: 'auto' }}
+            src={'/uploads/1556542894640.jpg'}
+            alt={'Image'}
+            loading={'lazy'}
+          />
+        </div>
+        <p>
+          Flipboard's PWA offers a fast and engaging experience for browsing news and other content,
+          with features such as offline mode and push notifications.
+        </p>
+
+        <h4>3. Pinterest</h4>
+        <div className={styles.img}>
+          <Image
+            width="0"
+            height="0"
+            sizes="100vw"
+            style={{ width: '75%', height: 'auto' }}
+            src={'/uploads/1556542894641.jpg'}
+            alt={'Image'}
+            loading={'lazy'}
+          />
+        </div>
+        <p>
+          Pinterest's PWA offers a fast and engaging user experience, with features such as push
+          notifications and an immersive full-screen mode.
+        </p>
+
+        <h4>4. Starbucks</h4>
+        <div className={styles.img}>
+          <Image
+            width="0"
+            height="0"
+            sizes="100vw"
+            style={{ width: '75%', height: 'auto' }}
+            src={'/uploads/1556542894642.jpg'}
+            alt={'Image'}
+            loading={'lazy'}
+          />
+        </div>
+        <p>
+          Starbucks' PWA allows users to order ahead and pay using their mobile device, as well as
+          providing features such as store locator and menu browsing.
+        </p>
 
         <h3>Final Thoughts</h3>
 
@@ -140,98 +205,6 @@ export default function Post() {
           the camera or accelerometer. However, as the technology continues to evolve, it's likely
           that more and more functionality will be added to PWAs, making them an even more
           compelling option for developers and businesses alike.
-        </p>
-        <h5>What do you want to achieve?</h5>
-        <ul>
-          <li>Website Traffic?</li>
-          <li>Lead Generation?</li>
-          <li>Build brand awareness?</li>
-        </ul>
-
-        <p>Clear goals will help you guide the rest of the social media strategy.</p>
-
-        <h3>2. Identify Targeted Audience</h3>
-        <p>
-          Understanding your target audience will help you choose the right social media platforms
-          to focus on and create content that resonates with them .
-        </p>
-
-        <h5>To undetify target audience we should use these questions:</h5>
-        <ul>
-          <li>Who are my target customers?</li>
-          <li>What social media platforms do they use?</li>
-        </ul>
-        <p>These answers are going to help while moving any further of this blog.</p>
-
-        <h3>3. Choose Social Media Platforms</h3>
-        <p>
-          Once targeted audience is known, choose the social media platforms that are most relevant
-          to them.
-        </p>
-        <p>
-          For example, if your target audience is primarily professionals, then LinkedIn may be a
-          better choice than Instagram.
-        </p>
-
-        <h3>4. Develop Content Strategy</h3>
-        <p>
-          Content Strategy should include the types of content you will create and share, the
-          frequency of posting, and the tone and style of the content.
-        </p>
-        <p>Please, consider the type of content that perfoms well on each platform.</p>
-
-        <h3>5. Create a Posting Schedule</h3>
-        <p>
-          Creating a social media posting schedule will insure that your company consistently post
-          content.
-        </p>
-
-        <div className={styles.img}>
-          <Image
-            width="0"
-            height="0"
-            sizes="100vw"
-            style={{ width: '75%', height: 'auto' }}
-            src={'/uploads/1556542894630.jpg'}
-            alt={'Image'}
-            loading={'lazy'}
-          />
-        </div>
-        <p>Consistency is the key in growing solid social media presence.</p>
-        <p>Determine how often your company will post.</p>
-
-        <div className={styles.note}>
-          <span>
-            Important to know what time is the best to make a post for your audience to see.
-          </span>
-          <br />
-          <br />
-          <span>
-            {`Posting at the wrong time could lead to being in the bottom of all the new posts that other's has made.`}
-          </span>
-        </div>
-
-        <h3>6. Engage with Your Audience</h3>
-        <p>Social media is a two-way conversation.</p>
-        <p>Engage with your audience by responsing to comments, messages and mentions.</p>
-        <p>This helps build relationships and increases brand loyalty.</p>
-
-        <h3>7. Measure and Analyze Your Results</h3>
-        <p>{`Finally, measuring and analyzing social media results regularly will give a taste of what's working and what's not.`}</p>
-
-        <p>
-          Using analytics tools to track engagement, traffic, and conversions from social media will
-          help improving your social media strategy.
-        </p>
-
-        <h3>Final thoughts</h3>
-        <p>
-          While creating strong goals, defining the presence in social media, measuring and
-          analyzing the progress will result in inevitable progression.
-        </p>
-        <p>
-          Following each step creates a winning social media strategy that drives traffic to your
-          website, builds brand awareness, and engages your target audience.
         </p>
       </section>
     </BlogWrapper>
