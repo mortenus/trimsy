9d537fb5b66a043812078db728e0754646599056
unknown <morten.hihij96@gmail.com>
2023-06-26 13:53:22 +0300

    Search Blog Logic

--- components/LocalNav/index.tsx
+++ components/LocalNav/index.tsx
@@ -3,13 +3,46 @@ import React from 'react';
 
 import styles from './LocalNav.module.scss';
 import clsx from 'clsx';
+import { useRouter } from 'next/router';
 
 const localNav = () => {
   const [searchVisible, setSearchVisible] = React.useState(false);
 
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  React.useEffect(() => {
+    if (searchVisible) {
+      inputRef.current && inputRef.current.focus();
+    }
+  }, [searchVisible]);
+
   const handleSearchVisible = () => {
     setSearchVisible(!searchVisible);
   };
+
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const router = useRouter();
+
+  const handleSearch = (event: any) => {
+    event.preventDefault();
+    router.push(`/blog/search?q=${searchQuery}`);
+  };
+
+  const handleKeyDown = (event: any) => {
+    if (event.key === 'Enter') {
+      handleSearch(event);
+    }
+  };
+
+  React.useEffect(() => {
+    console.log(router.pathname, router.query.q);
+    if (router.pathname === '/blog/search' && router.query.q) {
+      setSearchQuery(router.query.q.toString());
+    } else {
+      setSearchQuery('');
+      setSearchVisible(false);
+    }
+  }, [router.pathname, router.query.q]);
 
   return (
     <div className={styles[`intro-alternative`]}>
@@ -38,7 +71,14 @@ const localNav = () => {
                 <span>Search Blog</span>
               </div>
               <div className={clsx(styles[`search-active`], { [styles.visible]: searchVisible })}>
-                <input type="text" placeholder={'Search Article'} />
+                <input
+                  ref={inputRef}
+                  type="text"
+                  placeholder={'Search Article'}
+                  value={searchQuery}
+                  onChange={(event) => setSearchQuery(event.target.value)}
+                  onKeyDown={handleKeyDown}
+                />
 
                 {/* <Button color={'white'} size="small" type={'search'} onClick={() => {}}>
                   Search
