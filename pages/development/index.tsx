84f05aef8cc8ca38e8d52c54c91069a8e44d90bb
Mortenus <morten.hihiji96@gmail.com>
2024-02-18 21:28:15 -0700

    questions changed

--- pages/development/index.tsx
+++ pages/development/index.tsx
@@ -20,43 +20,43 @@ type TDevelopment = {
 
 const FAQArr = [
   {
-    title: 'How an online presence boosts businesses?',
-    description: `Having a strong online presence is crucial for businesses. An effective website showcases brand, products, and services to a vast online audience. It acts as a virtual storefront, allowing potential customers to learn about offerings at any time, from anywhere.`,
+    title: 'How an online presence boosts my businesses?',
+    description: `Having a strong online presence is crucial for businesses. An effective website showcases brand, products, and services to a vast online audience. It acts as a virtual storefront, allowing potential customers to learn about offerings at any time, anywhere.`,
   },
   {
-    title: 'What the difference between web design and development?',
+    title: 'What is the difference between design and development?',
     description: `Web Development is about software and web design is about visuals. The combination of this both practices provides a great end result \u2013 a well-monetized web application with several thousands of views.`,
   },
   {
     title:
-      'How does a user-friendly and intuitive website interface contribute to higher customer engagement and improved conversion rates?',
-    description: `Having a strong online presence is crucial for businesses. An effective website showcases brand, products, and services to a vast online audience. It acts as a virtual storefront, allowing potential customers to learn about offerings at any time, from anywhere.`,
+      'How does intuitive website makes a higher customer engagement and improved conversion rates?',
+    description: `Prioritizing user experience, interactivity, and responsive design can significantly and ultimately drive business to success in the digital landscape.`,
   },
   {
-    title: 'What is SEO?',
-    description: `SEO, or Search Engine Optimization, is the art and science of optimizing a website to rank higher on search engine results pages. It involves various techniques such as keyword research, content optimization, and link building to increase a website's visibility and attract more organic traffic.`,
+    title: 'What is Search Engine Optimization?',
+    description: `SEO, or Search Engine Optimization, is the art of optimizing a website to rank higher on search engine results pages. It involves various techniques such as keyword research, content optimization, and link building to increase a website's visibility and attract more organic traffic.`,
     learnMoreUrl: 'https://trimsy.org/blog/how-to-improve-seo-of-your-website-in-5-steps',
   },
   {
-    title: `How can analytics be used to improve a website's performance?`,
+    title: `How can analytics improve a website's performance?`,
     description: `Analytics can be used to improve a website's performance by providing valuable insights into user behavior and preferences. By analyzing data on website traffic, page views, bounce rates, conversion rates, and other metrics, website owners can make data-driven decisions to improve the user experience, optimize website design and content, identify areas for improvement, and ultimately increase engagement and conversion.`,
     learnMoreUrl: 'https://trimsy.org/blog/setting-up-google-analytics-fast-and-easy',
   },
   {
-    title: 'What Technlogoies do you use whilst developing an Application?',
-    description: `Website could be developed in so many ways, without using many technologies to most modern Libraries and Frameworks. We select them based on the application requirements to boost and make your web page look the way you want it any way possible. Our team has practical experience in applying most popular libraries while developing. ReactJS under NextJS is mostly focused upon due to its high reliance and popularity. While Backend or 'server-side' is focused on pure NodeJS (or NestJS) to ensure fast and secure exchange between the sides.`,
+    title: 'What technologies do you use when developing?',
+    description: `Website could be developed in many ways. We select specific library or framework based on the application requirements to boost and make your web page look the way you want it. Our team has practical experience in applying most popular libraries while developing. ReactJS under NextJS is mostly focused upon due to its high reliance and popularity. While Backend or 'server-side' is focused on pure NodeJS (or NestJS) to ensure fast and secure exchange between the sides.`,
   },
   {
     title:
-      'How does a professionally developed website improve your online reputation and build positive brand associations?',
+      'How does a website improves your online reputation and builds positive brand associations?',
     description: `A professionally developed website improves your online reputation by creating a positive first impression, showcasing your brand's professionalism and credibility. It builds positive brand associations by reflecting your brand identity, values, and unique selling points, fostering trust and loyalty among your target audience.`,
   },
   {
     title: 'What is Domain and VPS?',
-    description: `It is simple, to host your application, you would need to use Virtual Private Server, it is simply a private computer that runs your application. Domain is the link in the browser to direct user to that computer so they could experience the app in the browser. We provide such service, to get more details get in contact with us.`,
+    description: `To host your application, you would need to use Virtual Private Server (VPS), it is a private computer that runs your application. Domain is the link in the browser to direct user to that computer so they could see the app in the browser. We provide such service, to get more details get in contact with us.`,
   },
   {
-    title: 'What is website security and what measures can be taken to ensure a website is secure?',
+    title: 'What is website security and what measures can I take to ensure a website is secure?',
     description: `Website security refers to the protection of a website from unauthorized access, use, modification, or destruction. It involves implementing measures to prevent cyberattacks and ensure the confidentiality, integrity, and availability of the website and its data. To ensure a website is secure, measures such as using strong passwords, keeping software up to date, using HTTPS encryption, implementing firewalls, conducting regular backups, and using security plugins can be taken.`,
     learnMoreUrl:
       'https://trimsy.org/blog/website-security-how-to-protect-your-website-and-gain-a-comptetive-edge',
@@ -68,8 +68,8 @@ const FAQArr = [
   },
 ];
 
-Development.title = 'Website Development and Design';
-Development.description = 'The web So you can do the things you love in ways never before possible.';
+Development.title = 'Web Development and Design';
+Development.description = '';
 
 export default function Development({
   handleFormChange,
